cda0d0b6e113f61c16dfb6dc2f5e29b1738c5395
lxrdrgnbrn <65499326+lxrdrgnbrn@users.noreply.github.com>
2021-02-05 13:39:07 +0900

    Del

--- Lena-main/qt-projects/VidgetsProgram/Logic.cpp
+++
@@ -1,7 +0,0 @@
-float ForceGravity(float a,float b,float c)//Функция расчёта силы притяжения
-{
-    float g,f;
-    g=6.67;//Гравитационная постоянная
-    f=(a*b*g)/(c*c);
-    return f;
-}
--- Lena-main/qt-projects/VidgetsProgram/Logic.h
+++
@@ -1,4 +0,0 @@
-#ifndef LOGIC_H
-#define LOGIC_H
-float ForceGravity(float a,float b,float c);//Функция для расчёта силы притяжения
-#endif // LOGIC_H
